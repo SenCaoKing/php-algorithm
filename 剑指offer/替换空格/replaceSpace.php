453b35042673495042966d71fd880fde5fbf7daa
SenCaoKing <1134349927@qq.com>
2019-02-26 19:05:55 +0800

    剑指offer系列之 替换空格(思路分析)

--- 剑指offer/替换空格/replaceSpace.php
+++ 剑指offer/替换空格/replaceSpace.php
@@ -1,47 +1,51 @@
 <?php
 /**
- * 二维数组中的查找
+ * 替换空格
  *
- * 题目描述 : 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * 题目描述 : 请实现一个函数，将一个字符串中的每个空格替换成 "%20"。例如，当字符串为 We Are Happy 则经过替换之后的字符串为 We%20Are%20Happy.
  *
  */
 
-function find($target, $array)
+function replaceSpace($str)
 {
-    $i = count($array[0]) - 1;
-    $j = 0;
-
-    if ($array[count($array) - 1][$i] < $target || $array[0][0] > $target) {
+    // 原字符串长度
+    $len = strlen($str);
+    if ($len <= 0) {
         return false;
     }
-
-    for ($i; $i >= 0; $i--) {
-        if ($array[$j][$i] <= $target) {
-            for ($j; $j < count($array); $j++) {
-                if ($array[$j][$i] == $target) {
-                    return true;
-                } else if ($array[$j][$i] > $target) {
-                    break;
-                }
-            }
+    // 空格数量
+    $blank = 0;
+    for ($i = 0; $i < $len; $i++) {
+        if ($str[$i] == ' ') {
+            $blank++;
+        }
+    }
+    // 此处还需判断
+    if ($blank == 0) {
+        return false;
+    }
+    // 替换后字符串键值
+    $new_length = ($len + $blank * 2) - 1;
+    // 从后面往前面替换
+    for ($i = $len - 1; $i >= 0; $i--) {
+        if($str[$i] == ' ') {
+            $str[$new_length--] = '0';
+            $str[$new_length--] = '2';
+            $str[$new_length--] = '%';
+        } else {
+            $str[$new_length--] = $str[$i];
         }
     }
 
-    return false;
+    return $str;
 }
 
-$s = [[1, 2, 8, 9], [2, 4, 9, 12], [4, 7, 10, 13], [6, 8, 11, 15]];
-var_dump($s);
-
-var_dump(find(1, $s));
-var_dump(find(7, $s));
-var_dump(find(11, $s));
+var_dump(replaceSpace('We Are Happy'));
 
 /**
  * 思路：
  *
- * ① 把每一行看成是有序递增的数组，利用二分法查找。通过遍历每一行得到答案。时间复杂度是 nlogn
+ * ① 从前往后替换，后面的字符要不断往后移动，要多次移动，所以效率低下。
  *
- * ② 利用二维数组由上到下，由左到右递增的规律，那么选取右上角或者左下角的元素 $array[$row][$col] 与 $target 进行比较，当 $target 小于元素 $array[$row][$col] 时，那么 $target 必定在元素 $array 所在行的左边，即 $col--; 当 $target 大于 $array[$row][$col] 时，那么 $target 必定在元素 $array 所在列的下边，即 $row++;
+ * ② 从后往前，先计算需要多少空间，然后从后往前移动，则每个字符只移动一次，这样效率更高一点。
  */
-
