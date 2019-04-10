77804727d386636883e50d36c982b0f07b4876a2
SenCaoKing <1134349927@qq.com>
2019-04-10 22:26:42 +0800

    剑指offer系列之 搜索二进制中1的个数

---
+++ 剑指offer/搜索二进制中1的个数/binarySearchOne.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * 搜索整数二进制表示中1的个数
+ */
+
+// 方法一：只适用于正数
+function binarySearch1($n)
+{
+    if($n < 0){
+        exit("无法计算负数。");
+    }
+    $count = 0;
+    while($n){
+        if($n & 1){
+            $count++;
+        }
+        $n = $n >> 1;
+    }
+    return $count;
+}
+
+// 方法二：左移比较位（正负皆可）
+function binarySearch2($n)
+{
+    $count = 0;
+    $flag = 1;
+    while($flag){
+        if($n & $flag){
+            $count++;
+        }
+        $flag = $flag << 1;
+    }
+    return $count;
+}
+
+// 方法三：更高效，只比较二进制位次数
+function binarySearch3($n)
+{
+    $count = 0;
+    if($n < 0){
+        if(PHP_INT_SIZE == 4){
+            $n = $n & 0x7ffffffff;
+        }else{
+            $n = $n & 0x7fffffffffffffff;
+        }
+        $count++;
+    }
+    while($n){
+        $count++;
+        $n = ($n - 1) & $n;
+    }
+    return $count;
+}
+
+// test
+echo binarySearch1(12); // 2
+echo '<br />';
+
+echo binarySearch2(-12); // 29
+echo '<br />';
+
+echo binarySearch3(12); // 2
