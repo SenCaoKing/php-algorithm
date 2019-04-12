16334710f1b93ac1fb872aaf3c578eef40b737de
SenCaoKing <1134349927@qq.com>
2019-04-12 23:01:39 +0800

    剑指offer系列之 单例模式修改完善

--- 剑指offer/单例模式/singleton.php
+++ 剑指offer/单例模式/singleton.php
@@ -21,9 +21,51 @@ class A
     public function getInstance()
     {
         if(!isset(static::$instance)){
-            static::$instance = new static;
+            static::$instance = new static();
         }
         return static::$instance;
+    }
+}
+
+/**
+ * 单例
+ *
+ * Class A2
+ */
+class A2
+{
+    private static $instance = null;
+
+    public static function getInstance()
+    {
+        if(null === static::$instance){
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * 防止使用 new 创建多个实例
+     *
+     * A2 constructor.
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * 防止 clone 多个实例
+     */
+    private function __clone()
+    {
+    }
+
+    /**
+     * 防止反序列化
+     */
+    private function __wakeup()
+    {
     }
 }
 
